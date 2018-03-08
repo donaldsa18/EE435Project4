36a1cb5bbf5f08e6ed2f0399e7d848e144578f65
Anthony Donaldson <Anthony Donaldson>
2018-03-07 19:42:21 -0800

    Initial commit

---
+++ news.sv
@@ -0,0 +1,45 @@
+
+module newspaper(R,N1,D1,D2,Q,D,N,clk);
+import states::*;
+output R,N1,D1,D2;
+input Q,D,N,clk;
+reg [3:0] state,nextstate;
+reg R,N1,D1,D2;
+initial begin
+	state <= 3'b000;
+	nextstate <= 3'b000;
+	R <= 1'b0;
+	N1 <= 1'b0;
+	D1 <= 1'b0;
+	D2 <= 1'b0;
+end
+
+always @(posedge clk) begin
+	case(state)
+	S0: 	if(Q)      begin nextstate <= S25; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+		else if(D) begin nextstate <= S10; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+		else if(N) begin nextstate <= S5;  R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+	S5:	if(Q)      begin nextstate <= S30; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+		else if(D) begin nextstate <= S15; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+		else if(N) begin nextstate <= S10; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+	S10:	if(Q)      begin nextstate <= S0;  R <= 1'b1; {N1,D1,D2} <= 3'b000; end
+		else if(D) begin nextstate <= S20; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+		else if(N) begin nextstate <= S15; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+	S15:	if(Q)      begin nextstate <= S0;  R <= 1'b1; {N1,D1,D2} <= 3'b000; end
+		else if(D) begin nextstate <= S25; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+		else if(N) begin nextstate <= S20; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+	S20:	if(Q)      begin nextstate <= S0;  R <= 1'b1; {N1,D1,D2} <= 3'b010; end
+		else if(D) begin nextstate <= S30; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+		else if(N) begin nextstate <= S25; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+	S25:	if(Q)      begin nextstate <= S0;  R <= 1'b1; {N1,D1,D2} <= 3'b110; end
+		else if(D) begin nextstate <= S0;  R <= 1'b1; {N1,D1,D2} <= 3'b000; end
+		else if(N) begin nextstate <= S30; R <= 1'b0; {N1,D1,D2} <= 3'b000; end
+	S30:	if(Q)      begin nextstate <= S0;  R <= 1'b1; {N1,D1,D2} <= 3'b001; end
+		else if(D) begin nextstate <= S0;  R <= 1'b1; {N1,D1,D2} <= 3'b100; end
+		else if(N) begin nextstate <= S0;  R <= 1'b1; {N1,D1,D2} <= 3'b000; end
+	endcase
+end
+always @(negedge clk) begin
+	state <= nextstate;
+end
+endmodule
---
+++ states.sv
@@ -0,0 +1,3 @@
+package states;
+typedef enum {S0,S5,S10,S15,S20,S25,S30} state_t;
+endpackage
