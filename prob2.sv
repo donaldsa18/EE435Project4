fd59fffbfc6b79262ce96b709fdadfaa748345f0
U-Anthony-PC\Anthony <donaldsa18@up.edu>
2018-03-26 13:03:26 -0700

    Fixed typo in problem 2

--- prob2.sv
+++ prob2.sv
@@ -24,7 +24,7 @@ module prob2(Z1,Z2,X,clk);
 		S1: begin nextstate = X ? S2 : S1; {Z1,Z2} = 2'b00; end
 		S2: begin nextstate = X ? S3 : S4; {Z1,Z2} = X ? 2'b00 : 2'b10; end
 		S3: begin nextstate = X ? S3 : S4; {Z1,Z2} = 2'b00; end
-		S4: begin nextstate = X ? S3 : S5; {Z1,Z2} = X ? 2'b00 : 2'b01; end
+		S4: begin nextstate = X ? S2 : S5; {Z1,Z2} = X ? 2'b00 : 2'b01; end
 		S5: begin nextstate = X ? S6 : S5; {Z1,Z2} = 2'b00; end
 		S6: begin nextstate = X ? S6 : S7; {Z1,Z2} = 2'b00; end
 		S7: begin nextstate = X ? S6 : S5; {Z1,Z2} = X ? 2'b00 : 2'b01; end
